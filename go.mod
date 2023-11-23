31e5c9513090e15213c06e00ed6e935b589dbecf
Alyne Silva <alynensilva@gmail.com>
2023-11-23 20:52:37 -0300

    Chore: add comment and new main.go file

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module github.com/alynensilva/Go_Studies
+module mymodule
 
 go 1.21.4
+
+require github.com/alynensilva/puppy v0.0.0-20231120010238-c603dde7af2e
---
+++ main/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/alynensilva/puppy"
+)
+
+func main() {
+	fmt.Println("Hello World!")
+	fmt.Println(puppy.Bark())
+}
+
+
